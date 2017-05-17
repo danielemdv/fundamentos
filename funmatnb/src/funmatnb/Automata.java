ba7e70687da7b96591cd60f13c5028dcdff4c701
Daniel Espinosa <gangsterdan@gmail.com>
2017-05-17 17:25:26 -0500

    Completar funcion de creacion de metaestados

--- funmatnb/src/funmatnb/Automata.java
+++ funmatnb/src/funmatnb/Automata.java
@@ -2,6 +2,7 @@ package funmatnb;
 
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Automata{
 
@@ -38,16 +39,6 @@ public boolean esDeterminista(){
 
 
 public void determinar(){
-/*
-determinar metaestados (detMetaestados)
-Crear tabla intermedia.
-Derivar de esa la tabla final
-*/
-
-}
-
-//volver al automata determinista si es que no lo es
-public ArrayList<String> detMetaestados(){
   //Checa primero si es o no es determinsta. En caso de que no lo sea, procede con la determinacion
   /*
     if(this.esDeterminista()){
@@ -57,10 +48,27 @@ public ArrayList<String> detMetaestados(){
   */
 
 
-  Estado[] edos = (Estado[])estados.values().toArray();
-  ArrayList<String> metaEstados = new ArrayList();
+/*
+determinar metaestados (detMetaestados)
+Crear tabla intermedia.
+Derivar de esa la tabla final
+*/
 
-  metaEstados.add(edos[0].id); //Agregamos el primer estado, pues siempre es el de entrada
+}
+
+//volver al automata determinista si es que no lo es
+public ArrayList<String[]> detMetaestados(){
+/*Regresa un ArrayList de tipo String[2]. La primera entrada es el string del metaestado y la
+  segunda es un String que es "1" si el metaestado es de aceptacion, si no es "0"
+*/
+
+
+  Estado[] edos = (Estado[])estados.values().toArray();
+  ArrayList<String[]> metaEstados = new ArrayList();
+
+  //Agregamos el primer estado, pues siempre es el de entrada. Agregamos si es o no de aceptacion (en este proyecto no se puede)
+  String[] s = {edos[0].id,((edos[0].edoAceptacion) ? ("1"):("0"))};
+  metaEstados.add(s);
 
   boolean flagVacio = false; //Flag que se debe poner en true para saber si se debe agregar el estado vacio.
 
@@ -69,14 +77,26 @@ public ArrayList<String> detMetaestados(){
 
     //variables para los metaestados
     String x0 = "";
+    String x0Aceptation = "0";
     String x1 = "";
+    String x1Aceptation = "0";
 
     for (int j = 0; j < edos[i].x0destino.size() ; j++) {
       x0 += edos[i].x0destino.get(j);
+
+      //Si alguno de los estados agregados es de aceptacion, todo el metaestado es de aceptacion tambien
+      if(estados.get(edos[i].x0destino.get(j)).edoAceptacion){
+        x0Aceptation = "1";
+      }
+
     }
 
     for (int j = 0; j < edos[i].x1destino.size() ; j++) {
       x1 += edos[i].x1destino.get(j);
+
+      if(estados.get(edos[i].x1destino.get(j)).edoAceptacion){
+        x1Aceptation = "1";
+      }
     }
 
     //Si alguna transicion estaba vacia, activar la flag del estado vacio
@@ -84,29 +104,44 @@ public ArrayList<String> detMetaestados(){
       flagVacio = true;
     }
 
+    //Hacer un ordenamiento lexicografico del metaestado (e.g. ACB -> ABC) para asegurar no repeticiones
+    x0 = ordenarLex(x0);
+    x1 = ordenarLex(x1);
 
     //Asegurar que no se repita un metaestado ya contemplado y que no sean vacios
     if(metaEstados.indexOf(x0) == -1 && !x0.isEmpty()){
-      metaEstados.add(x0);
+      String[] m0 = {x0, x0Aceptation};
+      metaEstados.add(m0);
     }
 
     if(metaEstados.indexOf(x1) == -1 && !x1.isEmpty()){
-      metaEstados.add(x1);
+      String[] m1 = {x1, x1Aceptation};
+      metaEstados.add(m1);
     }
 
+  }//end for
+
+  //Agregar el estado vacio solamente si es necesario
+  if(flagVacio){
+    if(metaEstados.indexOf("-") == -1){
+      String[] v = {"-", "0"};
+      metaEstados.add(v);
+    }
   }
 
-  if(flagVacio){
-    metaEstados.add("-");
-  }
 
   return metaEstados;
 
 }
 
 
-
-
+//Método para regresar un String ordenado de manera lexicografica.
+public String ordenarLex(String s){
+  char[] chars = s.toCharArray();
+  Arrays.sort(chars);
+  String sorted = new String(chars);
+  return sorted;
+}
 
 
 
